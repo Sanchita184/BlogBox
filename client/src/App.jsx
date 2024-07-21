1d8bb14a0e64b2405834bd8d10fb5f73f5f55e43
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2024-07-21 22:18:13 +0530

    react router dom setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,27 @@
-import React, { useEffect } from 'react'
 import Homepage from './components/Homepage/Homepage'
 import Navbar from './components/Navbar/Navbar'
 import { useSelector } from 'react-redux'
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 const App = () => {
   const darkMode = useSelector(state => state.theme.darkMode)
 
   return (
-    <div className={`h-screen w-full flex items-center flex-col ${darkMode ? 'dark' : ''}`}>
-      <div className='h-full w-full flex items-center flex-col dark:bg-darkBg'>
-        <div className='md:w-2/3 lg:h-full lg:w-1/2'>
-          <Navbar />
-          <Homepage />
+    <BrowserRouter>
+      <div className={`h-screen w-full flex items-center flex-col ${darkMode ? 'dark' : ''}`}>
+        <div className='h-full w-full flex items-center flex-col dark:bg-darkBg'>
+          <div className='md:w-2/3 lg:h-full lg:w-1/2'>
+            <Navbar />
+            <Routes>
+              <Route path='/' element={<Homepage />} />
+              <Route path='/profile' element={<Navbar />} />
+              <Route path='/my-feed' element={<Navbar />} />
+              <Route path='/explore' element={<Navbar />} />
+              <Route path='/create-blog' element={<Navbar />} />
+            </Routes>
+          </div>
         </div>
       </div>
-    </div>
+    </BrowserRouter>
   )
 }
 
